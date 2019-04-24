5a5b5e65e518da21dbe4b2b36c54850de13695dc
Alejandro Foglino <alefoglino@gmail.com>
2019-04-24 14:46:33 -0300

    Nuevos recursos y mejoras

--- Backend/utils/utils.js
+++ Backend/utils/utils.js
@@ -1,6 +1,6 @@
  class Utils {
     static getUrlByKey () {
-        const API_URL ="http://www.omdbapi.com/?i=tt3896198&apikey=";
+        const API_URL ="http://www.omdbapi.com/?apikey=";
         const API_KEY="d0b64143";
         const endpoint = `${API_URL}${API_KEY}`;
         return endpoint;
